376119a9ec4cdfb663620d3b82fb036d6e5757e5
aknambiar <aditya.k.nambiar@gmail.com>
2023-09-05 12:23:06 +0530

    Add tests for mail_scheduler_job.rb

--- spec/jobs/mail_scheduler_job_spec.rb
+++ spec/jobs/mail_scheduler_job_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MailSchedulerJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:appointment) { create(:appointment) }
+
+  it "schedules a mail if the appointment exists" do
+    expect { MailSchedulerJob.perform_now(appointment.id) }.to have_enqueued_mail(InvoiceMailer)
+  end
+
+  it "does not schedule a mail if the appointment is cancelled" do
+    expect { MailSchedulerJob.perform_now(nil) }.not_to have_enqueued_mail(InvoiceMailer)
+  end
 end
