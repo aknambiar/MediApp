c44c9f17ea050e32adcba8720090a504cd5d0119
aknambiar <aditya.k.nambiar@gmail.com>
2023-09-15 15:47:21 +0530

    Remove unnecessary variables

--- app/javascript/controllers/date_picker_controller.js
+++ app/javascript/controllers/date_picker_controller.js
@@ -2,27 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="date-picker"
 export default class extends Controller {
-  static targets = [ "carouselItem", "slotPicker", "slotButton", "carouselButton" ]
+  static targets = [ "carouselItem", "slotPicker", "carouselButton" ]
 
   connect() {
     this.DATES_PER_SLIDE = 3;
-    this.slides = this.carouselItemTargets
-    this.slotButtons = this.slotPickerTargets
-    this.individualSlotButtons = this.slotButtonTargets
+    this.carouselItemTargets[0].classList.add('active');
+    this.slotPickerTargets[0].classList.remove('d-none');
 
     this.duplicateCarouselSlides();
-
-    this.carouselButtons = this.carouselButtonTargets
     this.addButtonListeners();
-
-    this.slides[0].classList.add('active');
-    this.setUnderline(this.carouselButtons[0]);
-    this.slotButtons[0].classList.remove('d-none');
+    this.setUnderline(this.carouselButtonTargets[0]);
     this.enableButton();
   }
 
   duplicateCarouselSlides() {
-    this.slides.forEach((slide) => {
+    this.carouselItemTargets.forEach((slide) => {
         let nextSlide = slide.nextElementSibling;
         for (var i = 1 ; i < this.DATES_PER_SLIDE ; i++ ) {
             if (!nextSlide) {
@@ -37,11 +31,11 @@ export default class extends Controller {
 }
 
 addButtonListeners() {
-    this.carouselButtons.forEach((button) => {
+    this.carouselButtonTargets.forEach((button) => {
         button.addEventListener('click', (event) => {
             this.setUnderline(button);
             let id = button.parentNode.firstElementChild.value;
-            this.slotButtons.forEach((element) => element.classList.add('d-none'));
+            this.slotPickerTargets.forEach((element) => element.classList.add('d-none'));
             let selectedDateSlots = document.getElementById(id);
             selectedDateSlots.classList.remove('d-none');
         })
@@ -50,13 +44,13 @@ addButtonListeners() {
 
 setUnderline(target) {
     if (window.screen.width > 992 ){
-        this.carouselButtons.forEach((button) => button.parentNode.classList.remove ("border", "border-primary", "border-5", "rounded", "fw-bolder", "fs-7", "bg-carousel"));
-        this.carouselButtons.forEach((button) => button.parentNode.classList.add ("border-bottom", "border-3", "fs-10"));
+        this.carouselButtonTargets.forEach((button) => button.parentNode.classList.remove ("border", "border-primary", "border-5", "rounded", "fw-bolder", "fs-7", "bg-carousel"));
+        this.carouselButtonTargets.forEach((button) => button.parentNode.classList.add ("border-bottom", "border-3", "fs-10"));
         target.parentNode.classList.add ("border-bottom", "border-primary", "border-5", "fw-bolder");
     }
     else {
-        this.carouselButtons.forEach((button) => button.parentNode.classList.remove ("border-bottom", "border-3", "border-primary", "border-5", "fw-bolder", "fs-10", "bg-carousel"));
-        this.carouselButtons.forEach((button) => button.parentNode.classList.add ("border", "rounded", "fs-7"));
+        this.carouselButtonTargets.forEach((button) => button.parentNode.classList.remove ("border-bottom", "border-3", "border-primary", "border-5", "fw-bolder", "fs-10", "bg-carousel"));
+        this.carouselButtonTargets.forEach((button) => button.parentNode.classList.add ("border", "rounded", "fs-7"));
         target.parentNode.classList.add ("border-primary", "bg-carousel");
     }
 }
