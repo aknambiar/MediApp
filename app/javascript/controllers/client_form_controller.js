a8d6075f30376231bd479ef7b2bc71e936f77d6d
aknambiar <aditya.k.nambiar@gmail.com>
2023-09-15 17:00:07 +0530

    Update event listeners to data-action attributes

--- app/javascript/controllers/client_form_controller.js
+++ app/javascript/controllers/client_form_controller.js
@@ -1,33 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="client-form"
 export default class extends Controller {
-  static targets = [ "currencyButton", "paymentButton"]
+  static targets = [ "currencyButton", "emailField", "emailError", "form", "paymentButton"];
 
-  connect() {
-    this.addSubmitButtonSpinner();
-    this.addButtonListeners();
+  updatePaymentButton() {
+    let rate = event.currentTarget.firstElementChild.getAttribute('rate');
+    rate = (Math.round(rate * 100) / 100).toFixed(2);
+    let currency = event.currentTarget.firstElementChild.getAttribute('value');
+    this.paymentButtonTarget.value = `Pay ${rate} ${currency}`;
   }
 
-  addButtonListeners() {
-    this.currencyButtonTargets.forEach((button) => {
-        button.addEventListener('click', (event) => {
-            let rate = button.firstElementChild.getAttribute('rate');
-            rate = (Math.round(rate * 100) / 100).toFixed(2);
-
-            let currency = button.firstElementChild.getAttribute('value');
-            
-            this.paymentButtonTarget.value = `Pay ${rate} ${currency}`;
-        })
-    })
-  }
-
-  addSubmitButtonSpinner() {
-    let spinner = this.paymentButtonTarget.nextElementSibling;
-    let form = document.querySelector('form');
-    form.addEventListener('submit', (event) => {
+  validateForm() {
+    if (this.validateEmail(this.emailFieldTarget.value)) {
+      let spinner = this.paymentButtonTarget.nextElementSibling;
       this.paymentButtonTarget.classList.add("d-none");
-        spinner.classList.remove("d-none");
-    })
+      spinner.classList.remove("d-none");
+      this.formTarget.submit();
+    }
+    else {
+      this.emailErrorTarget.classList.remove("d-none");
+    }
+  }
+
+  validateEmail(email) {
+    return email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   }
 }
